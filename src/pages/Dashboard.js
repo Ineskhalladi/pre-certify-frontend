35096ca8ef9a8037b12e11915ad13ded66432914
Ineskhalladi <khnoussa14@gmail.comm>
2025-04-27 00:38:52 +0100

    zdet 5demt

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -44,8 +44,92 @@ const Dashboard = () => {
     <>
       <NavBar2 />
       <div className="dashboard-charts-wrapper">
-        {/* Bar Chart */}
-        <div className="bar-chart-container">
+      <div className="first-line">
+      <div className="domaines-box">
+    <h2 className="domaines-title">Domaine acquis</h2>
+    <div className="domaines-list">
+      <div className="domaine-item">Santé et Sécurité au Travail</div>
+      <div className="domaine-item">Risques Industriels et autres</div>
+      <div className="domaine-item">Environnement</div>
+      <div className="domaine-item">Qualité</div>
+      <div className="domaine-item">Responsabilité Sociétale de Entreprise</div>
+    </div>
+    <div className="autres-domaines">+ Autres domaines</div>
+  </div>
+
+  <div className="pie-chart-container2">
+    <div className="pie-chart-title">Textes 2026</div>
+    <div className="pie-chart-wrapper">
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            data={textes2026Data}
+            cx="50%"
+            cy="50%"
+            outerRadius={90}
+            innerRadius={45}
+            dataKey="value"
+            labelLine={false}
+          >
+            {textes2026Data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend
+            layout="horizontal"
+            verticalAlign="top"
+            align="center"
+            iconType="circle"
+            formatter={(value) => (
+              <span style={{ color: "#000", fontSize: 14 }}>{value}</span>
+            )}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+      <div className="centered-text">{totalTextes2026}</div>
+    </div>
+  </div>
+
+  <div className="pie-chart-container">
+    <div className="pie-chart-title">Conformité générale</div>
+    <div className="pie-chart-wrapper2">
+      <ResponsiveContainer width="100%" height={300}>
+        <PieChart>
+          <Pie
+            data={pieData}
+            cx="40%"
+            cy="50%"
+            outerRadius={90}
+            dataKey="value"
+            labelLine={false}
+          >
+            {pieData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={entry.color} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend
+            verticalAlign="middle"
+            align="right"
+            layout="vertical"
+            iconType="circle"
+            formatter={(value) => (
+              <span style={{ color: "#000", fontSize: 18, marginLeft: 10 }}>
+                {value}
+              </span>
+            )}
+          />
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
+  </div>
+</div>
+
+<div className="second-line">
+
+         {/* Bar Chart */}
+         <div className="bar-chart-container">
           <div className="bar-chart-title">Quantité</div>
           <div className="bar-chart-wrapper">
             <ResponsiveContainer width="100%" height="100%">
@@ -69,72 +153,58 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className="pie-chart-container">
-  <div className="pie-chart-title">Textes 2026</div>
-  <div className="pie-chart-wrapper">
-    <ResponsiveContainer width="100%" height={300}>
-      <PieChart>
-        <Pie
-          data={textes2026Data}
-          cx="50%"
-          cy="50%"
-          outerRadius={90}
-          innerRadius={45}
-          dataKey="value"
-          labelLine={false}
-        >
-          {textes2026Data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={entry.color} />
-          ))}
-        </Pie>
-        <Tooltip />
-        <Legend
-          layout="vertical"
-          verticalAlign="middle"
-          align="right"
-          iconType="circle"
-          formatter={(value) => (
-            <span style={{ color: "#000", fontSize: 14 }}>{value}</span>
-          )}
-        />
-      </PieChart>
-    </ResponsiveContainer>
-    <div className="centered-text">{totalTextes2026}</div>
-  </div>
-</div>
-
-        {/* Pie Chart */}
-        <div className="pie-chart-container">
-          <div className="pie-chart-title">Conformité générale</div>
-          <div className="pie-chart-wrapper2">
-            <ResponsiveContainer width="100%" height={300}>
-              <PieChart>
-                <Pie
-                  data={pieData}
-                  cx="50%"
-                  cy="50%"
-                  outerRadius={80}
-                  dataKey="value"
-                  labelLine={false}
-                >
-                  {pieData.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={entry.color} />
-                  ))}
-                </Pie>
-                <Tooltip />
-                <Legend
-                  verticalAlign="middle"
-                  align="right"
-                  layout="vertical"
-                  iconType="circle"
-                  formatter={(value) => <span style={{ color: "#000" }}>{value}</span>}
-
+         {/* Bar Chart */}
+         <div className="bar-chart-container">
+          <div className="bar-chart-title">Risques Industriels et autres</div>
+          <div className="bar-chart-wrapper">
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                data={barData}
+                margin={{ top: 20, right: 10, left: 10, bottom: 50 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                <XAxis
+                  dataKey="name"
+                  interval={0}
+                  angle={-25}
+                  textAnchor="end"
+                  tick={{ fill: "#000",fontSize: 12 }}
                 />
-              </PieChart>
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="value" barSize={35} />
+              </BarChart>
             </ResponsiveContainer>
           </div>
         </div>
+
+        <div className="bar-chart-container">
+          <div className="bar-chart-title">Environnement</div>
+          <div className="bar-chart-wrapper">
+            <ResponsiveContainer width="100%" height="100%">
+              <BarChart
+                data={barData}
+                margin={{ top: 20, right: 10, left: 10, bottom: 50 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" vertical={false} />
+                <XAxis
+                  dataKey="name"
+                  interval={0}
+                  angle={-25}
+                  textAnchor="end"
+                  tick={{ fill: "#000",fontSize: 12 }}
+                />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="value" barSize={35} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+        </div>
+        </div>
       </div>
+      <p className="footer-base">Copyright © 2025 PreCertify. Tous les droits réservés.</p>
+
     </>
   );
 };
